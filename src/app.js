e05fabc6607d3f9977928fa28c3ed04c4bdebe27
Giovana Nogueira <ongiovana2016@gmail.com>
2023-11-23 13:31:32 -0300

    main update

---
+++ src/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(
+    8080, 
+    () =>{
+        console.log('o servidor está rodando na porta 8080')
+    }
+)
+
+app.get("/", (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get("/login", (req, res) => {
+    res.render('login.ejs');
+});
+
+app.get("/home", (req, res) => {
+    res.render('home.ejs');
+});
+
+app.get("/reservas", (req, res) => {
+    res.render('reservas.ejs');
+});
+
+app.get("/funcionarios", (req, res) => {
+    res.render('funcionarios.ejs');
+});
+
+app.get("/hospedes", (req, res) => {
+    res.render('hospedes.ejs');
+});
+
+app.get("/adicionar", (req, res) => {
+    res.render('adicionar.ejs');
+});
+
+
+app.post('/login', (req, res) => {
+    const password = req.body.password;
+  
+    // Lógica de verificação da senha (substitua pela sua lógica real)
+    if (password === '123') {
+        res.render('home.ejs');
+    } else {
+        res.render('login.ejs');
+    }
+  });
